950d2a39a067f1ae142f507c8dd24e364aaca89b
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-02 21:59:35 +0530

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int p,a;
+	p=2*(7+5);
+	printf("perimeter of the rectangle is:%d units\n",p);
+	a=7*5;
+	printf("area of the rectangle is:%d square units\n",a);
+}
