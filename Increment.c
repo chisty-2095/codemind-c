481538f8373667fc3075a377acb630ba335c16e1
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-14 18:58:10 +0530

    Increment.c

---
+++ Increment.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void increment()
+{
+	int a = 5;
+	int b = 5;
+	int prefix = ++a;
+	printf("Prefix Increment: %d\n", prefix);
+	int postfix = b++;
+	printf("Postfix Increment: %d", postfix);
+}
+
+
+int main()
+{
+	increment();
+	return 0;
+}
