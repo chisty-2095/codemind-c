ff92a1ce18ed2a53b74a5c5b5ec0009f04f7bc22
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-09-17 22:57:59 +0530

    Create concatenate_Two_Strings.c

---
+++ concatenate_Two_Strings.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string str1 = "Mohammad";
+	string str2 = "Khaja";
+	string result = str1 + str2;
+	cout << result << endl;
+	return 0;
+}
