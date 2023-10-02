b2372beeffea48532c87e331bad4e11864a61098
chisty-2095 <119730182+chisty-2095@users.noreply.github.com>
2023-10-02 22:18:15 +0530

    Char_to_int.c

---
+++ Char_to_int.c
@@ -0,0 +1,12 @@
+// C program to demonstrate conversion of
+// char to int using typecasting
+#include <stdio.h>
+ 
+// Driver code
+int main()
+{
+    char ch = 'g';
+    int N = (int)(ch);
+    printf("%d", N);
+    return 0;
+}
